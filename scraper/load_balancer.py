23babe682b48687a476d34ee7679ca7492fe22b7
celsea <celseamega@gmail.com>
2025-12-02 21:08:34 +0700

    reorganize functions

--- scraper/load_balancer.py
+++ scraper/load_balancer.py
@@ -20,15 +20,3 @@ class LoadBalancer:
         except Exception:
             logger.exception('Error while selecting scraper engine')
             return None
-        
-def clean_price(price):
-    """"Clean price string from non-numeric characters"""  
-    if isinstance(price, int):
-        return price
-    
-    if isinstance(price, str):
-        cleaned = ''.join(c for c in price if c.isdigit())
-        return int(cleaned) if cleaned else 0
-    
-    else:
-        return 0
